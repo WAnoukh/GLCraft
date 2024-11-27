244a8d4ca5892246e259b3bf4607b2e32b5e640e
AnoukhW <anoukhman@gmail.com>
2024-11-27 20:07:32 +0100

    Removing altlas mimap generation that cause crash

--- src/Application/Window.cpp
+++ src/Application/Window.cpp
@@ -12,7 +12,7 @@ int Window::init() {
     // ------------------------------
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     #ifdef __APPLE__
@@ -21,7 +21,7 @@ int Window::init() {
 
     // glfw window creation
     // --------------------
-    window = glfwCreateWindow(application.getScreenWidth(), application.getScreenHeight(), "LearnOpenGL", NULL, NULL);
+    window = glfwCreateWindow(application.getScreenWidth(), application.getScreenHeight(), "GLCraft", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
--- src/Rendering/TextureLoader.cpp
+++ src/Rendering/TextureLoader.cpp
@@ -43,16 +43,19 @@ unsigned int loadTexture(char const* path){
 
 unsigned int loadAtlas(char const* path) {
     unsigned int textureID;
-    glGenTextures(1, &textureID);
-
     int width, height, nrComponents;
     unsigned char* data = stbi_load(path, &width, &height, &nrComponents,0);
-    unsigned char* data2 = new unsigned char[width * height * 4];
+    if (data == NULL)
+    {
+        std::cout << "Image failed to load in." << std::endl;
+    }
+    
     //if (width != 512 || height != 512) {
     //    throw "loadAtlas error : wrong size !";
     //}
-    if (data)
+    if (data != NULL)
     {
+        unsigned char* data2 = new unsigned char[width * height * 4];
         GLenum format = GL_RGBA;
         if (nrComponents == 1)
             format = GL_RED;
@@ -60,40 +63,46 @@ unsigned int loadAtlas(char const* path) {
             format = GL_RGB;
         else if (nrComponents == 4)
             format = GL_RGBA;
-        glBindTexture(GL_TEXTURE_2D_ARRAY, textureID);
+        
         const GLsizei nb_tile = 32;
         size_t count = 0;
         for (size_t tl = 0; tl < nb_tile; ++tl) {
             for (size_t tc = 0; tc < nb_tile; ++tc) {
-                    for (size_t y = 0; y < 16; ++y) {
-                for (size_t x = 0; x < 16; ++x) {
-                        for (size_t colors = 0; colors < 4; ++colors) {
-                            data2[count] = data[(tl * 16 * 16 * nb_tile + tc * 16 + y * 16 * nb_tile + x)*4 + colors];
-                            ++count;
+                for (size_t y = 0; y < 16; ++y) {
+                    for (size_t x = 0; x < 16; ++x) {
+                            for (size_t colors = 0; colors < 4; ++colors) {
+                                data2[count] = data[(tl * 16 * 16 * nb_tile + tc * 16 + y * 16 * nb_tile + x)*4 + colors];
+                                ++count;
+                            }
                         }
                     }
-                }
             }
         }
-
-        glTexImage3D(GL_TEXTURE_2D_ARRAY, 0, format, 16, 16, 16 * 16, 0, format, GL_UNSIGNED_BYTE, data2);
-
-        glGenerateMipmap(GL_TEXTURE_2D_ARRAY);
-
+        glGenTextures(1, &textureID);
+        glBindTexture(GL_TEXTURE_2D_ARRAY, textureID);
+        glTexImage3D(GL_TEXTURE_2D_ARRAY, 0, GL_RGBA, 16, 16, 16 * 16, 0, GL_RGBA, GL_UNSIGNED_BYTE, data2);
+        
+        //glGenerateMipmap(GL_TEXTURE_2D_ARRAY);
+        
         glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_S, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_T, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
-        //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        //glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-        stbi_image_free(data);
+        //get opengl error
+        GLenum err;
+        while ((err = glGetError()) != GL_NO_ERROR) {
+            std::cout << "OpenGL error: " << err << std::endl;
+        }
+        
         delete[] data2;
     }
     else
     {
         std::cout << "Texture failed to load at path: " << path << std::endl;
-        stbi_image_free(data);
+        
     }
-
+    stbi_image_free(data);
     return textureID;
 }
